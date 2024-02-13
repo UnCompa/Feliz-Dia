1c49a34186de6e6e08bbb1b7f0c7709f307c6988
UnCompa <125362398+UnCompa@users.noreply.github.com>
2024-02-13 11:46:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+var videos = document.querySelectorAll('.video-js');
+    videos.forEach(function(video){
+        videojs(video);
+    });
+
 document.addEventListener("DOMContentLoaded", function() {
   var audio = document.getElementById("audioPlayer");
 
   // Función para reproducir automáticamente el audio
   function autoplayAudio() {
     if (audio.paused) {
-        audio.currentTime = 5
+        audio.currentTime = 10
         audio.volume = 0.3
         audio.play();
     }
@@ -30,6 +35,16 @@ button.addEventListener('click', ()=>{
 })
 
 document.addEventListener('DOMContentLoaded', function() {
+    new simpleParallax(document.querySelectorAll('#img2'), {
+        scale: 1.2, 
+        orientation: 'right', // Dirección del paralaje (up, down, left, right)
+        delay: 0.6, // Retardo de la animación en segundos
+        overflow: true, // Habilitar el desplazamiento del paralaje fuera de los límites de su contenedor
+        customContainer: null // Contenedor personalizado del paralaje (por defecto, usa el contenedor más cercano)
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
     new simpleParallax(document.querySelectorAll('#img1'), {
         scale: 1.2, 
         orientation: 'left', // Dirección del paralaje (up, down, left, right)
@@ -38,3 +53,40 @@ document.addEventListener('DOMContentLoaded', function() {
         customContainer: null // Contenedor personalizado del paralaje (por defecto, usa el contenedor más cercano)
     });
 });
+
+
+function calcularDiasTranscurridos(fechaInicio) {
+      // Fecha actual
+      var fechaActual = new Date();
+
+      // Convertir la fecha de inicio a milisegundos
+      var inicio = fechaInicio.getTime();
+
+      // Convertir la fecha actual a milisegundos
+      var actual = fechaActual.getTime();
+
+      // Calcular la diferencia de tiempo en milisegundos
+      var diferencia = actual - inicio;
+
+      // Convertir la diferencia de tiempo de milisegundos a días
+      var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+      return dias;
+    }
+
+    function actualizarDiasTranscurridos() {
+      // Fecha de inicio (15 de junio de 2023)
+      var fechaInicio = new Date(2023, 5, 15); // Meses en JavaScript se indexan desde 0 (0 = enero, 1 = febrero, etc.)
+
+      // Calcular los días transcurridos
+      var diasTranscurridos = calcularDiasTranscurridos(fechaInicio);
+
+      // Mostrar los días transcurridos en el elemento HTML
+      document.getElementById('counter').textContent = diasTranscurridos;
+    }
+
+    // Llamar a la función para mostrar los días transcurridos al cargar la página
+    actualizarDiasTranscurridos();
+
+    // Actualizar los días transcurridos cada día
+    setInterval(actualizarDiasTranscurridos, 24 * 60 * 60 * 1000); // Cada 24 horas (en milisegundos)
